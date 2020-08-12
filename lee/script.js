3e9fde2de0be26eb0ec00f26e12785b5ef3a7437
DOLSOTBAP <leejeongro@LEEJEONGROui-MacBookPro.local>
2020-08-12 15:50:39 +0900

    re

--- lee/script.js
+++ lee/script.js
@@ -2,6 +2,10 @@
 
 // DATA는 이미 작성된 트윗을 표시합니다.
 console.log(DATA)
+function oldTweet () {
+    let tmpl = document.querySelector('.eachComment');
+    let liElement = document.querySelector('li');
+}
 
 // generateNewTweet을 호출할 때마다 새로운 트윗을 생성합니다.
 console.log(generateNewTweet());
